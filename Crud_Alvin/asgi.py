8cbef40408aa04f7b9575dd08336f84edc0bf2c0
ngarealvin18 <ngarealvin18@gmail.com>
2024-03-05 09:09:34 +0300

    initial commit

---
+++ Crud_Alvin/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Crud_Alvin project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Crud_Alvin.settings')
+
+application = get_asgi_application()
---
+++ Crudapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+    path('detail/<int:id>/', views.task_detail, name='task_detail'),
+    path('create/', views.task_create, name='task_create'),
+    path('updste/<int:id>/', views.task_edit, name='task_edit'),
+    path('delete/<int:id>/', views.task_delete, name='task_delete'),
+
+
+
+]
